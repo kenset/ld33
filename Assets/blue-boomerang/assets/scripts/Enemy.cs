c1aad3b7338fee684ce5df2adcb4c50357da8eda
Trey Sedate <tsedate@gmail.com>
2015-08-23 02:01:10 -0500

    Adds ability for enemies to become agressive on seeing the player

--- Assets/blue-boomerang/assets/scripts/Enemy.cs
+++ Assets/blue-boomerang/assets/scripts/Enemy.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class Enemy : MessageBehaviour {
 
 	public float timeToLive = 10.0f;
+	public float sightDistance = 2.0f;
+
 	private bool timerActive = false;
 
 	private enum Awareness {
@@ -21,11 +23,33 @@ public class Enemy : MessageBehaviour {
 		GetComponent<Renderer>().enabled = false;
 		GetComponent<BoxCollider2D>().enabled = false;
 	}
-	
+
+	void FixedUpdate() {
+		Collider2D[] inSight = Physics2D.OverlapCircleAll(transform.position, sightDistance);
+
+		foreach (Collider2D seenObject in inSight) {
+			if (seenObject.gameObject.tag.Equals("Player")) {           
+				Vector3 direction = (seenObject.transform.position - transform.position).normalized;
+
+//				RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, sightDistance);
+//				if (hit.collider.gameObject.tag.Equals("Player")) {                             
+//					Debug.DrawRay(transform.position, direction, Color.red);                                         
+
+					if (Vector3.Angle(transform.up, direction) > 90) {
+						awareness = Awareness.Agressive;
+						GetComponent<SimpleAI2D>().Player = seenObject.transform;
+//						print ("seen");
+					}
+//				}
+//				print ("unseen");
+			}
+		}
+	}
+
 	protected override void OnStart () {
 		awareness = Awareness.Unaware;
 	}
-
+	
 	void Update () {
 		if (timerActive) {
 			updateTimer();
