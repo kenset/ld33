12f79944eb78dfc9acf8e0455b3db78c3b914b21
Trey Sedate <tsedate@gmail.com>
2015-08-23 10:23:21 -0500

    Adds ability for player to not be seen if behind another object

--- Assets/blue-boomerang/assets/scripts/Enemy.cs
+++ Assets/blue-boomerang/assets/scripts/Enemy.cs
@@ -5,6 +5,9 @@ public class Enemy : MessageBehaviour {
 
 	public float timeToLive = 10.0f;
 	public float sightDistance = 2.0f;
+	public float sightAngle = 90.0f;
+
+	public bool debug = false;
 
 	private bool timerActive = false;
 
@@ -25,23 +28,41 @@ public class Enemy : MessageBehaviour {
 	}
 
 	void FixedUpdate() {
+
+		// Retrieve a list of objects that are in the enemies aggression zone.
 		Collider2D[] inSight = Physics2D.OverlapCircleAll(transform.position, sightDistance);
 
+		// For every object that the enemy has the potential to see...
 		foreach (Collider2D seenObject in inSight) {
-			if (seenObject.gameObject.tag.Equals("Player")) {           
+
+			// If the player is within the enemy's aggression zone...
+			if (seenObject.gameObject.tag.Equals("Player")) { 
+
+				// Calculate the direction between the enemy and the player.
 				Vector3 direction = (seenObject.transform.position - transform.position).normalized;
 
-//				RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, sightDistance);
-//				if (hit.collider.gameObject.tag.Equals("Player")) {                             
-//					Debug.DrawRay(transform.position, direction, Color.red);                                         
+				// If the angle between the enemy and the player is within the
+				// enemy's line of sight...
+				if (Vector3.Angle(transform.up, direction) > sightAngle) {
 
-					if (Vector3.Angle(transform.up, direction) > 90) {
-						awareness = Awareness.Agressive;
-						GetComponent<SimpleAI2D>().Player = seenObject.transform;
-//						print ("seen");
+					// Check to see if anything is between the enemy and the player.
+					RaycastHit2D hit = Physics2D.Raycast(transform.position, direction);
+
+					if (debug) {
+						Debug.DrawRay(transform.position, direction * hit.distance, Color.red);
+						print (hit.collider.gameObject);
 					}
-//				}
-//				print ("unseen");
+
+					// If there is nothing between the enemy and the player...
+					if (hit.collider.gameObject.tag.Equals("Player")) {       
+
+						// Make the enemy aggressive.
+						awareness = Awareness.Agressive;
+
+						// Set the seen Player as the object of pursuit.
+						GetComponent<SimpleAI2D>().Player = seenObject.transform;
+					}
+				}
 			}
 		}
 	}
